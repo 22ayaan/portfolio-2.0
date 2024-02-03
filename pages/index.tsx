91541def5c6f7484cc9961d143f0deae0653c782
Ayaan Siddiqui <ars394@pitt.edu>
2024-02-02 20:58:58 -0500

    finished till about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import type { NextPage } from "next";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Ayaan Siddiqui - Portfolio</title>
       </Head>
@@ -13,15 +15,21 @@ const Home: NextPage = () => {
       <Header />
 
       {/* Hero */}
+      <section id="hero" className='snap-start'>
+        <Hero />
+      </section>
 
       {/* About */}
+      <section id="about" className='snap-center'>
+        <About />
+      </section>
 
       {/* Experience */}
 
       {/* Projects */}
 
       {/* Contact */}
-    </>
+    </div>
   );
 };
 
