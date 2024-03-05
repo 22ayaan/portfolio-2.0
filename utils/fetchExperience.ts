08c86d3ef0dc55808af4269ad67262b36bc3fa6c
Ayaan Siddiqui <ars394@pitt.edu>
2024-03-05 15:49:45 -0500

    finished adding utility funcs and sanity environment

---
+++ utils/fetchExperience.ts
@@ -0,0 +1,10 @@
+import { Experience } from "@/typings";
+
+export const fetchExperiences = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SANITY_BASE_URL}/api/getExperiences`
+  );
+  const data = await res.json();
+  const experiences: Experience[] = data.experiences;
+  return experiences;
+};
